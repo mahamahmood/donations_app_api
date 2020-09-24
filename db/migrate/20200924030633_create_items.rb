959e05f8e2312ac12d0ea39f367801294f0d8e56
Maha Mahmood <>
2020-09-23 23:48:59 -0400

    made rails server. db created and seeded. all controllers are tested in postman

---
+++ db/migrate/20200924030633_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :category
+      t.string :condition
+      t.string :img
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,58 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create([
+    {
+        "name": "Fancy Dress",
+        "category": "Clothes",
+        "condition": "Never Used",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    },
+    {
+        "name": "Computer Desk",
+        "category": "Office",
+        "condition": "Great",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    },
+    {
+        "name": "HP Monitor 27 Inch",
+        "category": "Electronics",
+        "condition": "Good",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    },
+    {
+        "name": "Chair",
+        "category": "Office",
+        "condition": "Not Bad",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    },
+    {
+        "name": "Black Shirt",
+        "category": "Clothes",
+        "condition": "Good",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    },
+    {
+        "name": "Wireless Mouse",
+        "category": "Electronics",
+        "condition": "Good",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    },
+    {
+        "name": "Flower Dress",
+        "category": "Clothes",
+        "condition": "Never Used",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    },
+    {
+        "name": "Hat",
+        "category": "Clothes",
+        "condition": "Good",
+        "img": "http://coolspotters.com/files/photos/591533/ryan-gosling-large.jpg?1357449177"
+    }
+]);
